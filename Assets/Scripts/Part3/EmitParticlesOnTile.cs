5a38cbcca0f903c2564b8779623ac8b5728dd233
Sally Chen <jc11618@nyu.edu>
2023-11-15 02:07:06 -0500

    Particle systems added

--- Assets/Scripts/Part3/EmitParticlesOnTile.cs
+++ Assets/Scripts/Part3/EmitParticlesOnTile.cs
@@ -2,30 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EmitParticlesOnTile : MonoBehaviour
-{
 
+public class EmitParticlesOnTile: MonoBehaviour
+{
     public ParticleSystem ptcl;
     public string tileTag;
 
     void Start()
     {
-        ptcl.Stop();
+        ptcl.Stop(false);
     }
-    
-    void OnTriggerEnter2D(Collider2D col)
+
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == tileTag)
+      if(col.tag == tileTag)
         {
-            ptcl.Play();
+            ptcl.Play(false);
         }
     }
 
-    void OnTriggerExit2D(Collider2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
-        if (col.tag == tileTag)
+       if(col.tag == tileTag)
         {
-            ptcl.Stop();
+            ptcl.Stop(false);
         }
     }
 }
